2ab542a19c6270b0d776f01e77865baa1d233d93
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-14 20:04:07 +0200

    Remove providers from app.php, they are again registered in their respective packages composer.json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,9 @@
 <?php
 
 use Latus\Helpers\Paths;
-use Latus\Installer\Providers\InstallerServiceProvider;
 use Latus\Laravel\Bootstrapper;
-use Latus\Latus\Providers\LatusServiceProvider;
-use Latus\Settings\Providers\SettingsServiceProvider;
-use Latus\UI\Providers\UIServiceProvider;
 
 $bootstrapper = new Bootstrapper(Paths::basePath());
-
-$bootstrapper->addBaseProviders([
-    SettingsServiceProvider::class,
-    UIServiceProvider::class,
-    InstallerServiceProvider::class
-]);
 
 $bootstrapper->build();
 
